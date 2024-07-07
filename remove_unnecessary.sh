b80b81086c49d6c8e10f6a26e0648ee9ecac0e4c
meshandmore <dryufei@gmail.com>
2024-07-07 16:56:58 +0800

    initial commit.

---
+++ remove_unnecessary.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Remove the Unnecessary
+# If you forked or downloaded the minimal-mistakes-jekyll repo you can safely remove the following folders and files:
+
+cd ../meshandmore.github.io &&\
+rm .editorconfig 
+rm .gitattributes 
+rm .github -r 
+rm docs -r 
+rm test -r 
+rm CHANGELOG.md 
+rm minimal-mistakes-jekyll.gemspec
+rm README.md 
+rm screenshot-layouts.png 
+rm screenshot.png
